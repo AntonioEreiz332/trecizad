e505be078af8a0d06ca6ac19f7dfe66f8db3b919
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-06-07 09:10:35 +0200

    Add files via upload

---
+++ trecizad/trecizad/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trecizad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gepard g = new Gepard();
+            Console.WriteLine(g.Opasnost());
+        }
+    }
+}
